dbd61f62545bc99818c22bf256760f4348690708
fined000@naver.com <fined000@naver.com>
2022-12-20 23:18:46 +0900

    second commit --add icons

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import { StyleSheet, Text, View, ScrollView, ActivityIndicator, Dimensions } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import * as Location from 'expo-location';
+import {Fontisto} from "@expo/vector-icons";
+
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const API_KEY = "c0dc9035ac6b6f3350dea3d41034a40b";
+const icons = {
+  "Clouds": "cloudy",
+  "Clear": "day-sunny",
+  "Snow": "snow",
+  "Rain": "rains",
+  "Drizzle": "rain",
+  "Thunderstorm": "lightning"
+}
 
 export default function App() {
 
@@ -15,21 +25,27 @@ export default function App() {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if(!granted){
       setOk(false);
+      // To Do List
+      return (
+        <View>
+        </View>
+      )
+    } else {
+      const { 
+        coords: {latitude, longitude},
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+      const location = await Location.reverseGeocodeAsync(
+        {latitude, longitude}, 
+        {useGoogleMaps: false}
+      );
+      setCity(location[0].city);
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+      )
+      const json = await response.json();
+      setDays(json.daily);
+      };
     }
-    const { 
-      coords: {latitude, longitude},
-    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
-    const location = await Location.reverseGeocodeAsync(
-      {latitude, longitude}, 
-      {useGoogleMaps: false}
-    );
-    setCity(location[0].city);
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
-    )
-    const json = await response.json();
-    setDays(json.daily);
-  };
 
   useEffect(() => {
     getWeather();
@@ -58,9 +74,12 @@ export default function App() {
           ) : (
             days.map((day, index) => (
               <View key={index} style={styles.day}>
-                <Text style={styles.temp}>
-                  {parseFloat(day.temp.day).toFixed(1)}
-                </Text>
+                <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%"}}>
+                  <Text style={styles.temp}>
+                    {parseFloat(day.temp.day).toFixed(1)}
+                  </Text>
+                  <Fontisto name={icons[day.weather[0].main]} size={70} color="white" />
+                </View>
                 <Text style={styles.description}>{day.weather[0].main}</Text>
                 <Text style={styles.tinyText}>{day.weather[0].description}</Text>
               </View>
@@ -85,22 +104,31 @@ const styles = StyleSheet.create({
   cityName: {
     fontSize: 58,
     fontWeight: "500",
+    color: "white"
   },
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingHorizontal: 20
   },
   temp: {
     marginTop: 50,
     fontWeight: "600",
-    fontSize: 178,
+    fontSize: 100,
+    color: "white"
   },
   description: {
-    marginTop: -30,
-    fontSize: 60,
+    marginTop: -10,
+    fontSize: 30,
+    color: "white",
+    fontWeight: "500"
   },
   tinyText: {
     fontSize: 20,
+    marginTop: -5,
+    fontSize: 25,
+    color: "white",
+    fontWeight: "500",
   },
 });
